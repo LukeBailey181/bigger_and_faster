2d0771654fe21f5ecadf81c90e51ed185060f98d
LukeBailey181 <lukebailey@college.harvard.edu>
2021-11-29 14:13:20 -0500

    add quant and non quant lat predictor

--- script/train_lat_predictor.sh
+++ script/train_lat_predictor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PROJECT_ROOT=/Users/lukebailey/Documents/ProDocuments/projects/CS242/bigger_and_faster
 LAT_DATASET_PATH=${PROJECT_ROOT}/conf_datasets/lat_quant_.tmp
-PATH_TO_SAVE_MODEL=${PROJECT_ROOT}/conf_datasets/lat_predictor.pt
+PATH_TO_SAVE_MODEL=${PROJECT_ROOT}/conf_datasets/lat_predictor_quant.pt
 
 python ${PROJECT_ROOT}/latency_predictor.py \
     --lat_dataset_path=${LAT_DATASET_PATH} \
